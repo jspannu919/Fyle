f080ce1ec76b2759d5bee1aec6aa54d12b364875
jspannu919 <jspannu919@gmail.com>
2021-01-19 22:57:46 +0530

    server port issur fix

--- src/server/server.js
+++ src/server/server.js
@@ -6,7 +6,7 @@ const appConfig = require("../../config/config");
 const NodeCache = require("node-cache");
 const cache = new NodeCache();
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 global.appConfig = appConfig;
 global.pool = dbConnection(appConfig.postgres);
