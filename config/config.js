05c8cdd22d7ca237fe71dd4436d9fc954951e5d9
jspannu919 <jspannu919@gmail.com>
2021-01-19 20:08:32 +0530

    update config file

--- config/config.js
+++ config/config.js
@@ -6,11 +6,7 @@ const config = {
     password: process.env.DATABASE_PASSWORD,
     port: process.env.DATABASE_PORT,
     max: process.env.DATABASE_MAX_CONNECTIONS,
-    schema: {
-      recruitment: process.env.RECRUITMENT_DB_SCHEMA,
-      patent: process.env.PATENT_DB_SCHEMA,
-      workersRecruitment: process.env.WORKERS_RECRUITMENT_DB_SCHEMA,
-    },
+    schema: "public",
   },
 };
 
